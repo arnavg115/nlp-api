3cd17b97ba896c6b1284a5145978c74dd47d0472
arnavg115 <soccercream20@gmail.com>
2021-10-09 03:05:24 +0000

    tf

--- app.py
+++ app.py
@@ -1,56 +1,21 @@
-from ariadne import QueryType, graphql_sync, make_executable_schema,ObjectType
-from ariadne.constants import PLAYGROUND_HTML
+
 from flask import Flask, request, jsonify
 import transformers
 
 
-query = QueryType()
-
 summarizer = transformers.pipeline("summarization")
 
-type_defs = """
-    type Query {
-        summarizer(article: String!): String!
-    }
-"""
 
-query = ObjectType("Query")
-
-@query.field("summarizer")
-def resolve_holidays(*_, article):
-    res = summarizer(article,min_length=30,max_length=100)
-    return res[0]['summary_text']
-
-schema = make_executable_schema(type_defs, query)
 
 app = Flask(__name__)
 
 
-@app.route("/graphql", methods=["GET"])
-def graphql_playground():
-    # On GET request serve GraphQL Playground
-    # You don't need to provide Playground if you don't want to
-    # but keep on mind this will not prohibit clients from
-    # exploring your API using desktop GraphQL Playground app.
-    return PLAYGROUND_HTML, 200
-
-
-@app.route("/graphql", methods=["POST"])
-def graphql_server():
-    # GraphQL queries are always sent as POST
-    data = request.get_json()
-
-    # Note: Passing the request to the context is optional.
-    # In Flask, the current request is always accessible as flask.request
-    success, result = graphql_sync(
-        schema,
-        data,
-        context_value=request,
-        debug=app.debug
-    )
-
-    status_code = 200 if success else 400
-    return jsonify(result), status_code
+@app.route("/", methods=["POST"])
+def main():
+   json:dict = request.get_json(force=True)
+   text = json.get("text")
+   res = summarizer(text,min_length=30,max_length=100) if text != None else {"error":True}
+   return jsonify(res)
 
 
 if __name__ == "__main__":
